1c82a4468bd420f708183f0282372999cbd79c70
luiz <luiz.silveira@mprj.mp.br>
2016-08-12 18:54:39 -0300

    Added a new language: Portuguese

--- src/client/app/frameworks/app/services/app-config.service.ts
+++ src/client/app/frameworks/app/services/app-config.service.ts
@@ -2,13 +2,14 @@
 import {ILang} from '../../core/index';
 
 export class AppConfigService {
-  
+
   public static SUPPORTED_LANGUAGES: Array<ILang> = [
     { code: 'en', title: 'English' },
     { code: 'es', title: 'Spanish' },
     { code: 'fr', title: 'French' },
     { code: 'ru', title: 'Russian' },
-    { code: 'bg', title: 'Bulgarian' }
+    { code: 'bg', title: 'Bulgarian' },
+    { code: 'pt', title: 'Portuguese' }
   ];
 
 }
