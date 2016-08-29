2bab4933ba6828de8eae624cb02c20817b67035f
Luiz Silveira <zertyz@gmail.com>
2016-08-29 05:51:50 -0300

    Adaptations to the latest upstream changes, part 2

--- src/client/app/components/app.routes.ts
+++ src/client/app/components/app.routes.ts
@@ -1,11 +1,33 @@
-// angular
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-// app
-import { HomeRoutes } from './home/home.routes';
-import { AboutRoutes } from './about/about.routes';
+import {AboutRoutes} from './about/about.routes';
+import {HomeRoutes} from './home/home.routes';
+
+import {GvHomeRoutes}                   from './pages/gv-home/gv-home.routes';
+import {GvNovaContaRoutes}              from './pages/gv-novaConta/gv-novaConta.routes';
+import {GvMenuRoutes}                   from './pages/gv-menu/gv-menu.routes';
+import {GvConfirmaPedidosRoutes}        from './pages/gv-confirmaPedidos/gv-confirmaPedidos.routes';
+import {GvEdicaoRoutes}                 from './pages/gv-edicao/gv-edicao.routes';
+import {GvEfetivaPedidosRoutes}         from './pages/gv-efetivaPedidos/gv-efetivaPedidos.routes';
+import {GvContaParcialRoutes}           from './pages/gv-contaParcial/gv-contaParcial.routes';
+import {GvContaParcialIndividualRoutes} from './pages/gv-contaParcialIndividual/gv-contaParcialIndividual.routes';
+import {GvContaParcialMesaRoutes}       from './pages/gv-contaParcialMesa/gv-contaParcialMesa.routes';
+import {GvFormaDePagamentoRoutes}       from './pages/gv-formaDePagamento/gv-formaDePagamento.routes';
+import {GvPesquisaSatisfacaoRoutes}     from './pages/gv-pesquisaSatisfacao/gv-pesquisaSatisfacao.routes';
 
 export const routes: Routes = [
   ...HomeRoutes,
-  ...AboutRoutes
+  ...AboutRoutes,
+
+  ...GvHomeRoutes,
+  ...GvNovaContaRoutes,
+  ...GvMenuRoutes,
+  ...GvConfirmaPedidosRoutes,
+  ...GvEdicaoRoutes,
+  ...GvEfetivaPedidosRoutes,
+  ...GvContaParcialRoutes,
+  ...GvContaParcialIndividualRoutes,
+  ...GvContaParcialMesaRoutes,
+  ...GvFormaDePagamentoRoutes,
+  ...GvPesquisaSatisfacaoRoutes,
 ];
